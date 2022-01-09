fb4af48da088526cdf9672d18a81b94e1485b852
thanhhafmcvn <83865348+MLucastero@users.noreply.github.com>
2022-01-09 11:07:25 +0700

    Add files via upload

---
+++ money.py
@@ -0,0 +1,32 @@
+while True:
+    print('''
+#     # ##    # ######      
+#     # # #   # #     # 
+#     # #  #  # #     # 
+ #   #  #   # # #     # 
+  # #   #    ## #     # 
+   #    #     # ######  
+            ''')
+    print("CTRL C to quit!")
+    # Input 
+    vnd = float(input("Enter VND: "))
+    usd = float(vnd) / 23000
+    euro = float(vnd) / 26000
+    yen = float(vnd) / 196
+    yuan = float(vnd) /  3500
+    print('')
+    # Calculate
+    print("==========================================")
+    print ( str(vnd) + " VND = " + str(usd) + " USD")
+    print("------------------------------------------")
+    print ( str(vnd) + " VND = " + str(euro) + " Euro")
+    print("------------------------------------------")
+    print ( str(vnd) + " VND = " + str(yen) + " Yen")
+    print("------------------------------------------")
+    print ( str(vnd) + " VND = " + str(yuan) + " Yuan")
+    print("==========================================")
+    print("^^^^^^@@@@@@^^^^^^******^^^^^^@@@@@@^^^^^^") 
+    
+   
+
+    
